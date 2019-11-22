d6e2d997f32c8768503a2819a6d1c9b12a95e7a9
Park June Chul <pjc0247@naver.com>
2019-11-22 11:23:27 +0900

    asdf

---
+++ functions/src/api/iap.ts
@@ -0,0 +1,29 @@
+import { NotAuthorizedError } from '../error';
+import { onHttpsCall } from '../common';
+import { User } from '../controller/user';
+import * as admin from 'firebase-admin';
+import * as iap from 'in-app-purchase';
+
+iap.config(require('../config/iap.json'));
+const iapSetupProm = iap.setup();
+
+async function waitForIapSetup(){
+    await iapSetupProm;
+}
+async function validateReceipt(receipt: string) {
+    await waitForIapSetup();
+    await iap.validate({});
+}
+
+export const iap_claim = onHttpsCall(async (data, ctx) => {
+    if (!ctx.auth) throw new NotAuthorizedError();
+
+    const receipt = data.receipt;
+
+    let user = User.get(ctx.auth.uid);
+    await user.ensureDataExistInLocal();
+
+    return {
+        user
+    };
+});
